b8e6af0764815758e65c951c0df456f95f96e467
Arnav Singh <92050142+arnav-s1ngh@users.noreply.github.com>
2024-04-24 19:23:21 +0530

    Update orientationdaointerface.kt

--- orientationdaointerface.kt
+++ orientationdaointerface.kt
@@ -2,12 +2,13 @@ package com.example.myapplication
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
 interface orientationdaointerface {
-    @Insert
-    suspend fun insert(orientationData: orientationdataentity)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertorientdata  (orientationData: orientationdataentity)
 
     @Query("SELECT * FROM orientation_data")
     fun getAllData(): List<orientationdataentity>
